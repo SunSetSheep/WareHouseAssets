6a464a7ceb6b349dcc5978e4a9fe8a5ee745696a
Lorraine Warren <lorrywarren@gmail.com>
2025-05-08 15:04:31 +0100

    Add Asset variables / objects and ensure user inputs are required.

--- WareHouseAssets/Models/Asset.cs
+++ WareHouseAssets/Models/Asset.cs
@@ -1,6 +1,24 @@
-﻿namespace WareHouseAssets.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WareHouseAssets.Models
 {
     public class Asset
     {
+        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int? Number_In_Stock { get; set; }
+        [Required]
+        public string? Supplier { get; set; }
+        [Required]
+        public string? Image { get; set; }
+        [Required]
+        public string? Department { get; set; }
+        [Required]
+        public DateOnly LastUpdated { get; set; }
+
+
     }
 }
